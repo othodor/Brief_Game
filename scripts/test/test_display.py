c2417b0cdb176fccea1811959378ea052bcb463f
Thanh Lan <ManonLan@users.noreply.github.com>
2021-11-17 13:06:37 +0100

    revise function and test for display.py

--- scripts/test/test_display.py
+++ scripts/test/test_display.py
@@ -10,4 +10,4 @@ sys.path.insert(0, parentdir)
 from display import display_info  
 
 def test_display():
-    assert display_info("Mark", 28, 1, 14) == "Mark, il vous reste 28 points de vie et 1 potion(s), l'ennemi a 14"
+    assert display_info("Mark", 28, 1, 14) == "Mark, il vous reste 28 points de vie et 1 potion(s), l'ennemi a 14 points de vie."
