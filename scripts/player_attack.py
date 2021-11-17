5c2fb100c99d6545ffafecc6bec28dc4eb062abb
othodor <othodor@gmail.com>
2021-11-17 13:32:11 +0100

    Update comments

--- scripts/player_attack.py
+++ scripts/player_attack.py
@@ -1,6 +1,6 @@
 import random
 
-""" This function remove a random number of pv between 5 and 10 to the troll"""
+""" This function reduce a random number of pv between 5 and 10 of the troll"""
 def player_attack(epv):
     attack_player = random.randint(5, 10)
     epv = epv - attack_player
--- scripts/welcome.py
+++ scripts/welcome.py
@@ -1,3 +1,5 @@
+""" This function ask for a pseudoname"""
+
 def welcome():
     player_name = input("Bienvenue dans ce Brief Game. Quel est votre pseudo?")
     print(f'Bonjour {player_name}, veux tu partir au combat ?')
